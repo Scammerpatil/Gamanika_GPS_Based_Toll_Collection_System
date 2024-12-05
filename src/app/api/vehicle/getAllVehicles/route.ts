74214f4b82604e56ea08ffd654b0b7672abe6a80
Saurav Patil <sauravpatil453@gmail.com>
2024-12-05 08:46:55 +0530

    value

--- src/app/api/vehicle/getAllVehicles/route.ts
+++ src/app/api/vehicle/getAllVehicles/route.ts
@@ -1,10 +1,14 @@
 import User from "@/models/User";
 import dbConfig from "@/middlewares/db.config";
 import { NextResponse } from "next/server";
+import Vehicle from "@/models/Vehicle";
 
 dbConfig();
 
 export async function GET() {
-  const users = await User.find().populate("vehicles");
+  const vehicle = await Vehicle.find();
+  if (!vehicle) {
+  }
+  const users = await User.find().populate("vehicle");
   return NextResponse.json(users, { status: 200 });
 }
